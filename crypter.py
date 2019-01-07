426d08d16937beb1439d2313447224d9e4aea55d
Pedro Alves <pedroalves706@gmail.com>
2019-01-07 22:19:07 +0000

    support path jumbling

--- crypter.py
+++ crypter.py
@@ -8,21 +8,21 @@ import os
 SETTINGS = {}
 
 # TODO check if gpg command succeeded before removing the file... 
-def encrypt_file(keys, file: str) -> None:
+def encrypt_file(keys, file) -> None:
     r = subprocess.run(["gpg", "--output", file+".enc", "--encrypt", "--recipient", keys, file])
     if r.returncode == 0:
         subprocess.run(["shred", "-uzn", "10", file])
     else:
-        print("Failed to encrypt file %s, not removing" % file, file=sys.stderr)
+        print(f"Failed to encrypt file {file!r}, not removing", file=sys.stderr)
 
-def decrypt_file(file: str, password):
+def decrypt_file(file, password):
     r = subprocess.run(["gpg", "--output", file[:-4], "--batch", "--passphrase", password, "--decrypt", file], capture_output=True)
     if r.returncode == 0:
         os.remove(file)
     else:
-        print("Failed to decrypt file %s, not removing" % file, file=sys.stderr)
+        print(f"Failed to decrypt file {file!r}, not removing", file=sys.stderr)
 
-def init(dir: str, symmetric=False, key=None):
+def init(dir, symmetric=False, key=None):
     pass
 
 def find_dir():
@@ -35,17 +35,72 @@ def find_dir():
     
     return None
 
-def lock_dir(key, path):
-    for path, _, files in os.walk(path):
+def lock_dir(key, _path):
+    for path, _, files in os.walk(_path):
         for file in files:
-            if not file.endswith(".enc") and file != ".crypter":
+            if not file.endswith(".enc") and file != ".crypter" and file != ".filenames":
                 encrypt_file(key, os.path.join(path, file))
+    if SETTINGS['jumble'] and not os.path.isfile(os.path.join(find_dir(), ".filenames.enc")):
+        jumble(_path)
+        encrypt_file(key, os.path.join(find_dir(), ".filenames"))
 
-def unlock_dir(path, passphrase):
-    for path, _, files in os.walk(path):
+def unlock_dir(_path, passphrase):
+    if SETTINGS['jumble'] and os.path.isfile(os.path.join(find_dir(), ".filenames.enc")):
+        decrypt_file(os.path.join(find_dir(), ".filenames.enc"), passphrase)
+        unjumble(_path)
+        os.remove(os.path.join(find_dir(), ".filenames"))
+
+    for path, _, files in os.walk(_path):
         for file in files:
             if file.endswith(".enc"):
                 decrypt_file(os.path.join(path, file), passphrase)
+    
+def unjumble(path):
+    names = []
+    basepath = find_dir()
+    with open(os.path.join(basepath, ".filenames"), 'r') as f:
+        for line in f:
+            l = line.strip()
+            if l != "":
+                names.append(l.split(":"))
+    
+    for name in names:
+        dir = os.path.join(basepath, os.path.dirname(name[1]))
+        finalpath = os.path.join(basepath, name[1])
+        curpath = os.path.join(basepath, name[0])
+        if not os.path.isdir(dir):
+            os.makedirs(dir)
+        os.rename(curpath, finalpath)
+
+def jumble(_path):
+    counter = 0
+    names = []
+    basepath = find_dir()
+    for path, _, files in os.walk(_path):
+        for file in files:
+            if file.endswith(".enc") and file != ".filenames.enc":
+                curpath = os.path.join(path, file)
+                newpath = str(counter) + ".enc"
+                names.append((newpath, os.path.relpath(curpath, basepath)))
+
+                os.rename(curpath, os.path.join(basepath, newpath))
+
+                counter += 1
+
+    delete_subdirs(basepath)
+
+    with open(os.path.join(basepath, '.filenames'), 'w') as f:
+        text = ""
+        for name in names:
+            text += f'{name[0]}:{name[1]}\n'
+        f.write(text)
+
+def delete_subdirs(path):
+    subdirs = next(os.walk(path))[1]
+    for dir in subdirs:
+        delete_subdirs(os.path.join(path, dir))
+        if os.listdir(os.path.join(path, dir)) == []:
+            os.rmdir(os.path.join(path, dir))
 
 def main():
     config = configparser.ConfigParser()
@@ -57,6 +112,7 @@ def main():
     config.read(os.path.join(dir, ".crypter"))
 
     SETTINGS['key'] = config.get('config', 'key', fallback=None)
+    SETTINGS['jumble'] = config.get('config', 'jumble', fallback=True)
     if SETTINGS['key'] == None:
         print("Key not specified in config.", file=sys.stderr)
     
